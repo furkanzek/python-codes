c3e68e711242dc54be7a7be74cc10cabc8dfcd25
Furkan Zekiri <furkanzekiri101@gmail.com>
2022-10-31 04:03:32 +0300

    son

--- PyCharm/pd_strs.py
+++ PyCharm/pd_strs.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
-data = pd.read_csv('nba.csv')
+data = pd.read_csv('C:/Users/furka/OneDrive/Masaüstü/python-codes/PyCharm/nba.csv')
 data.dropna(inplace=True)
 
 data['player_name'] = data['player_name'].str.upper()
+data['player_name'] = data['player_name'].str.lower()
+data['index'] = data['player_name'].str.find('f')
+data = data[data.player_name.str.contains('james')]
+data = data.team_abbreviation.str.replace('A', 'X').str.replace('S', 'W')
+data[['first_name', 'last_name']] = data['player_name'].loc[data['player_name'].str.len()==2].str.split()
 
-print(data)
+print(data.head(10))
